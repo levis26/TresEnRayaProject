38c9dceda1d16edb544ed9daef3a08a2442f399f
daw1 <lflores@elpuig.xeill.net>
2024-04-25 20:48:21 +0200

    Adding methods to class TUI

--- src/TUI.java
+++ src/TUI.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class TUI {
+
+
+    private Scanner sc;
+
+
+    public int mostrarMenu(){
+        return 1;
+    }
+
+    public void mostrarTaulell(char taullel, short torn){
+        System.out.println("Aqui mostramos la taula.");
+    }
+
+    public short recollirJugada(){
+        return 3;
+    }
+
+    public short fiDepartida(int guanydor){
+        return 4;
+    }
+
 }
