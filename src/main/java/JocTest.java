b0d33e77673341f72faafdd9dec8555ad0c1ab61
daw1 <lflores@elpuig.xeill.net>
2024-05-08 17:38:04 +0200

    Adding test jugadaGuanyadora_noGanadora and jugadaGuanyadora_noGanadora_unaCasilla to jugadaGuanyadora() method.

--- src/main/java/JocTest.java
+++ src/main/java/JocTest.java
@@ -1,4 +1,3 @@
-// Clase JocTest
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -6,13 +5,13 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class JocTest {
 
     @ParameterizedTest
-    @CsvSource({"0,0", "1,1", "2,2", "1,0", "1,1","1,2", "2,0", "2,1", "2,2"})
+    @CsvSource({"0,0", "0,1", "0,2", "1,0", "1,1", "1,2", "2,0", "2,1", "2,2"})
     void novaPartida_OK(int fila, int columna) {
         Joc joc = new Joc();
         joc.novaPartida();
         char[][] taulell = joc.getTaulell();
 
-        // Realizar la prueba sobre el tablero
+        // Verificar que todas las casillas están vacías
         Assertions.assertEquals('-', taulell[fila][columna]);
     }
 
@@ -30,19 +29,18 @@ public class JocTest {
 
     // Método para obtener la representación del tablero
     private String getTableroRepresentation(char[][] taulell) {
-        String representation = "";
-        for (int i = 0; i < taulell.length; i++) {
-            for (int j = 0; j < taulell[i].length; j++) {
-                representation += taulell[i][j] + " ";
+        StringBuilder representation = new StringBuilder();
+        for (char[] fila : taulell) {
+            for (char celda : fila) {
+                representation.append(celda).append(" ");
             }
-            representation += "\n";
+            representation.append("\n");
         }
-        return representation;
+        return representation.toString();
     }
 
-
     @ParameterizedTest
-    @CsvSource({"0,0", "1,1", "2,2","1,0", "1,1","1,2","2,0", "2,1", "2,2"})
+    @CsvSource({"0,0", "0,1", "0,2", "1,0", "1,1", "1,2", "2,0", "2,1", "2,2"})
     void jugar_OK(int fila, int columna) {
         Joc joc = new Joc();
         joc.novaPartida();
@@ -51,11 +49,10 @@ public class JocTest {
         // Verificar que el jugador ha colocado su ficha en la posición correcta
         char[][] taulell = joc.getTaulell();
         Assertions.assertEquals('O', taulell[fila][columna]);
-
     }
 
     @ParameterizedTest
-    @CsvSource({"0,0", "1,1", "2,2", "1,0", "1,1","1,2", "2,0", "2,1", "2,2"})
+    @CsvSource({"0,0", "0,1", "0,2", "1,0", "1,1", "1,2", "2,0", "2,1", "2,2"})
     void jugar_rep(int fila, int columna) {
         Joc joc = new Joc();
         joc.novaPartida();
@@ -64,9 +61,9 @@ public class JocTest {
         // Verificar que al menos una casilla se ha rellenado con 'O'
         char[][] taulell = joc.getTaulell();
         boolean oFound = false;
-        for (int i = 0; i < taulell.length; i++) {
-            for (int j = 0; j < taulell[i].length; j++) {
-                if (taulell[i][j] == 'O') {
+        for (char[] filaTablero : taulell) {
+            for (char celda : filaTablero) {
+                if (celda == 'O') {
                     oFound = true;
                     break;
                 }
@@ -77,5 +74,28 @@ public class JocTest {
         }
         Assertions.assertTrue(oFound, "Se esperaba que al menos una casilla estuviera rellenada con 'O'");
     }
+
+    @ParameterizedTest
+    @CsvSource({"0,0", "0,1", "0,2", "1,0", "1,1", "1,2", "2,0", "2,1", "2,2"})
+    void jugadaGuanyadora_noGanadora(int fila, int columna) {
+        Joc joc = new Joc();
+        joc.novaPartida();
+
+        // Verificar que no hay jugada ganadora en un tablero vacío
+        Assertions.assertFalse(joc.jugadaGuanyadora(fila, columna));
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({ "0,1", "0,2", "1,0", "1,1", "1,2", "2,0", "2,1", "2,2"})
+    void jugadaGuanyadora_noGanadora_unaCasilla(int fila, int columna) {
+        Joc joc = new Joc();
+        joc.novaPartida();
+        joc.jugar(0, 0);
+
+        // Verificar que no hay jugada ganadora en un tablero con una sola casilla ocupada
+        Assertions.assertFalse(joc.jugadaGuanyadora(fila, columna));
+    }
+
 
 }
