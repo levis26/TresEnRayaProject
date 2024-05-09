708f03fd211ceb35acff007112d1191c147b6248
irene <irene.urbano.luque@elpuig.xeill.net>
2024-05-09 19:31:02 +0200

    He añadido la jugada ganadora para el jugador 2

--- src/main/java/JocTest.java
+++ src/main/java/JocTest.java
@@ -1,5 +1,6 @@
 // Clase JocTest
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -72,7 +73,8 @@ public class JocTest {
     novaPartida y el metodo jugar. Después creamos un boolean que sea false, recorremos la
     matriz y si la posición en la que estamos es igual a "0" hacemos que el oFound (que es nuestro
     boolean) sea true, y se rompa el bucle y llame a nuestro boolean para que se rompa el bucle
-    y el Assertions lo que nos hace es ...
+    y el Assertions lo que nos hace es que si es True, salga un mensaje diciendo que al menos
+    tendria que haber una casilla con una 'O'
     */
 
     @ParameterizedTest
@@ -97,5 +99,37 @@ public class JocTest {
             }
         }
         Assertions.assertTrue(oFound, "Se esperaba que al menos una casilla estuviera rellenada con 'O'");
+    }
+
+    /*
+    Esto lo que nos va a hacer es buscar en el taulell de la posición [i][j] si hay un '-',
+    en caso de que sea verdadero, se va a escribir una 'X' y se va a cortar el bucle, en caso
+    contrario, vamos a volverlo a ejecutar, si no encuentra ningún '-', va a dar el boolean
+    false y se va a cortar el primer bucle
+    */
+    @ParameterizedTest
+    @CsvSource({"0,0", "0,1", "0,2", "1,0", "1,1","1,2", "2,0", "2,1", "2,2"})
+    void jugar_jugador2(int fila, int columna){
+        Joc joc = new Joc();
+        joc.novaPartida();
+        joc.jugar(fila, columna);
+
+        char[][] taulell = joc.getTaulell();
+        boolean posicionlibre = false;
+        for (int i = 0; i < taulell.length; i++) {
+            for (int j = 0; j < taulell[i].length; j++) {
+                if (taulell[i][j] == '-'){
+                    System.out.println('X');
+                    posicionlibre = true;
+                    break;
+                }
+
+            }
+            if (posicionlibre) {
+                break;
+            }
+        }
+
+        Assertions.assertTrue(posicionlibre, "Se ha colocado 'X'");
     }
 }
